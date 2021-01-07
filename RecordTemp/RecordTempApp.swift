b1dfbb4de7766bb46dfb23990bfe2a744535ba82
Ryosukekamimura <come1234560@gmail.com>
2021-01-07 09:54:38 +0900

    Initialize Repository

---
+++ RecordTemp/RecordTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordTempApp.swift
+//  RecordTemp
+//
+//  Created by 神村亮佑 on 2021/01/07.
+//
+
+import SwiftUI
+
+@main
+struct RecordTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
